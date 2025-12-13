c2a9f66a880e9f883856d584c2f973ae1e1a9f0b
loupde <15086835668@163.com>
2025-12-13 22:42:06 +0800

    edge_tts==7.2.1

--- main/manager-web/vue.config.js
+++ main/manager-web/vue.config.js
@@ -41,6 +41,7 @@ const useCDN = process.env.VUE_APP_USE_CDN === 'true';
 module.exports = defineConfig({
   productionSourceMap: process.env.NODE_ENV !=='production', // 生产环境不生成 source map
   devServer: {
+    allowedHosts: 'all',
     port: 8001, // 指定端口为 8001
     proxy: {
       '/xiaozhi': {
