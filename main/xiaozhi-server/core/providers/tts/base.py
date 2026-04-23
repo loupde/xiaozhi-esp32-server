af356c48b8b794887494ab47157c6d1770e86d19
loup <253204749@qq.com>
2026-04-23 10:06:03 +0800

    add log

--- main/xiaozhi-server/core/providers/tts/base.py
+++ main/xiaozhi-server/core/providers/tts/base.py
@@ -247,6 +247,7 @@ class TTSProviderBase(ABC):
                 sentence_id = str(uuid.uuid4().hex)
                 conn.sentence_id = sentence_id
         # 对于单句的文本，进行分段处理
+        logger.bind(tag=TAG).error(f"对于单句的文本，进行分段处理: {content_detail}")
         segments = re.split(r"([.。！？!?；;\n])", content_detail)
         for seg in segments:
             self.tts_text_queue.put(
